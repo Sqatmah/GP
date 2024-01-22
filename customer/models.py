9d94e3463a21614a56aa8fa19df41ece2218a00b
abedzalloum <abdalhafeez@sitech.me>
2024-01-22 22:17:35 +0300

    formatting the model file

--- customer/models.py
+++ customer/models.py
@@ -3,18 +3,21 @@ from django.contrib.auth.models import User
 from datetime import date
 from .constant import *
 
+
 class Base(models.Model):
     date_created = models.DateTimeField()
     date_updated = models.DateTimeField()
 
     class Meta:
-        abstract = True 
+        abstract = True
+
 
 class Role(Base):
     name = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f"{self.name}"
+
 
 class Address(Base):
     street = models.CharField(max_length=255, null=True, blank=True)
@@ -23,16 +26,17 @@ class Address(Base):
 
     def __str__(self) -> str:
         return f"{self.street} / {self.city} / {self.state}"
-    
+
+
 class Users(Base):
-    user=models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     role = models.OneToOneField(Role, on_delete=models.CASCADE, null=True, blank=True)
     address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
     name_company = models.CharField(
-        max_length=255, 
-        null=True, 
+        max_length=255,
+        null=True,
         blank=True
-        )
+    )
     date_of_birth = models.DateField(
         null=True,
         blank=True,
@@ -41,24 +45,24 @@ class Users(Base):
         max_length=255,
         null=False,
         blank=False,
-        choices = EX_SUBSCRIPTION
+        choices=EX_SUBSCRIPTION
     )
     phone = models.CharField(
         max_length=255,
         null=True,
         blank=True
-        )
+    )
     user_status = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        choices = USER_STATUS,
+        choices=USER_STATUS,
     )
     gender = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        choices = GENDERS,
+        choices=GENDERS,
     )
     blood = models.CharField(
         max_length=255,
@@ -70,10 +74,11 @@ class Users(Base):
         null=True,
         blank=True
     )
+
     def __str__(self) -> str:
         return f"{self.user.first_name} {self.user.last_name}"
 
-    
+
 class InsuranceCompany(Base):
     name = models.CharField(max_length=256, null=True, blank=True)
     logo = models.ImageField(
@@ -88,31 +93,33 @@ class InsuranceCompany(Base):
     establishment = models.DateField()
     degrees = models.CharField(
         max_length=255,
-        null=True, 
+        null=True,
         blank=True,
-        choices = INSURANCE_DEGREE
+        choices=INSURANCE_DEGREE
     )
+
     def __str__(self) -> str:
         return f"{self.name}"
-    
+
 
 class Payments(Base):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     insurance_company = models.OneToOneField(InsuranceCompany, on_delete=models.CASCADE, null=True, blank=True)
     amount = models.CharField(
         max_length=255,
-        null=True, 
+        null=True,
         blank=True
-        )
+    )
     payments_status = models.CharField(
         max_length=255,
         null=True,
         blank=True,
-        choices = PAYMENT_STATUS
+        choices=PAYMENT_STATUS
     )
 
     def __str__(self) -> str:
         return f"{self.user} paid for {self.insurance_company}"
+
 
 class Requests(Base):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -121,12 +128,12 @@ class Requests(Base):
         max_length=255,
         null=True,
         blank=True,
-        choices = REQUEST_STATUS
+        choices=REQUEST_STATUS
     )
 
     def __str__(self) -> str:
         return f"{self.user} request to {self.insurance_company} and status is {self.request_status}"
-    
+
 
 class MedicalProfile(Base):
     user = models.OneToOneField(Users, on_delete=models.CASCADE, null=True, blank=True)
@@ -143,7 +150,7 @@ class MedicalProfile(Base):
         blank=True
     )
     family_medical = models.TextField(
-        null=True, 
+        null=True,
         blank=True
     )
     history = models.TextField(
